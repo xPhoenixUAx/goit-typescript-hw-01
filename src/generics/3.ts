9b9fc6ac5ec956018f7248ef5ef0c7eaeef19512
Pavlo Mazur <161851925+xPhoenixUAx@users.noreply.github.com>
2024-11-02 02:19:19 +0200

    Update 3.ts

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,9 +1,10 @@
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 const merged = merge({ name: "Alisa" }, { age: 28 });
 
-merged.name;
+merged.name; 
+merged.age;
 
 export {};
